0f13a438a7e7e3f7c74ffe192575e3907b37f398
nath54 <cerisara.nathan@gmail.com>
2020-09-20 10:16:54 +0200

    aa

--- js/quiz_dispos.js
+++ js/quiz_dispos.js
@@ -1,7 +1,8 @@
 
 var quiz_dispos=[
     ["Les verbes a particules","vap"],
-    ["TF, agl grp 1, Voc anglais exo voc + The 6 boxes p197","english_1"]
+    ["TF, agl grp 1, Voc anglais exo voc + The 6 boxes p197","english_1"],
+    ["TF, esp grp 1, Voc espagnol theme inmigracion, la Bestia","esp_1"],
 ];
 
 
